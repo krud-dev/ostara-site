f6ac8f1d6992e5ce8f0b2d0bef9fc5ffbfba0678
avivm <mo.aviv@gmail.com>
2023-04-30 15:16:39 +0300

    feat: initial commit

---
+++ assets/js/theme-custom.js
@@ -0,0 +1,109 @@
+
+//Muse Toggle Class JavaScript
+document.querySelectorAll('.back-arrow').forEach(museSidebarToggler => {
+  museSidebarToggler.addEventListener('click', ({target}) => {
+    target.closest('#mainBox').classList.toggle('sidebar-icons');
+  });
+});
+
+//Muse Custom JavaScript
+function _objectWithoutProperties(source, excluded) { if (source == null) return {}; var target = _objectWithoutPropertiesLoose(source, excluded); var key, i; if (Object.getOwnPropertySymbols) { var sourceSymbolKeys = Object.getOwnPropertySymbols(source); for (i = 0; i < sourceSymbolKeys.length; i++) { key = sourceSymbolKeys[i]; if (excluded.indexOf(key) >= 0) continue; if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue; target[key] = source[key]; } } return target; }
+
+function _objectWithoutPropertiesLoose(source, excluded) { if (source == null) return {}; var target = {}; var sourceKeys = Object.keys(source); var key, i; for (i = 0; i < sourceKeys.length; i++) { key = sourceKeys[i]; if (excluded.indexOf(key) >= 0) continue; target[key] = source[key]; } return target; }
+
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+var docReady = function docReady(fn) {
+  if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', fn);
+  } else {
+    setTimeout(fn, 1);
+  }
+};
+
+//Muse Resize JavaScript
+var resize = function resize(fn) {
+  return window.addEventListener('resize', fn);
+};
+
+//Muse Getdata JavaScript
+var getData = function getData(el, data) {
+  try {
+    return JSON.parse(el.dataset[data]);
+  } catch (e) {
+    return el.dataset[data];
+  }
+};
+
+var isScrolledIntoView = function isScrolledIntoView(el) {
+  var top = el.offsetTop;
+  var left = el.offsetLeft;
+  var width = el.offsetWidth;
+  var height = el.offsetHeight;
+
+  while (el.offsetParent) {
+    el = el.offsetParent;
+    top += el.offsetTop;
+    left += el.offsetLeft;
+  }
+
+  return {
+    all: top >= window.pageYOffset && left >= window.pageXOffset && top + height <= window.pageYOffset + window.innerHeight && left + width <= window.pageXOffset + window.innerWidth,
+    partial: top < window.pageYOffset + window.innerHeight && left < window.pageXOffset + window.innerWidth && top + height > window.pageYOffset && left + width > window.pageXOffset
+  };
+};
+
+//Muse Color JavaScript
+var hexToRgb = function hexToRgb(hexValue) {
+  var hex;
+  hexValue.indexOf('#') === 0 ? hex = hexValue.substring(1) : hex = hexValue; // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+
+  var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex.replace(shorthandRegex, function (m, r, g, b) {
+    return r + r + g + g + b + b;
+  }));
+  return result ? [parseInt(result[1], 16), parseInt(result[2], 16), parseInt(result[3], 16)] : null;
+};
+var rgbaColor = function rgbaColor() {
+  var color = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : '#fff';
+  var alpha = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0.5;
+  return "rgba(".concat(hexToRgb(color), ", ").concat(alpha, ")");
+};
+var colors = {
+  primary: '#2c7be5',
+  secondary: '#748194',
+  success: '#00d27a',
+  info: '#27bcfd',
+  warning: '#f5803e',
+  danger: '#e63757',
+  light: '#f9fafd',
+  dark: '#0b1727'
+};
+var grays = {
+  white: '#fff',
+  100: '#f9fafd',
+  200: '#edf2f9',
+  300: '#d8e2ef',
+  400: '#b6c1d2',
+  500: '#9da9bb',
+  600: '#748194',
+  700: '#5e6e82',
+  800: '#4d5969',
+  900: '#344050',
+  1000: '#232e3c',
+  1100: '#0b1727',
+  black: '#000'
+};
+var utils = {
+  docReady: docReady,
+  resize: resize,
+  getData: getData,
+  hexToRgb: hexToRgb,
+  rgbaColor: rgbaColor,
+  isScrolledIntoView: isScrolledIntoView,
+  colors: colors,
+  grays: grays,
+};
+
